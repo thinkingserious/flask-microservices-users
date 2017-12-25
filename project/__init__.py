54a45450f2e97fedb43a488ba70c235f0446f764
Elmer Thomas <elmer@thinkingserious.com>
2017-12-25 08:15:53 -0800

    First commit

---
+++ project/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+# instantiate the app
+app = Flask(__name__)
+
+# set config
+app.config.from_object('project.config.DevelopmentConfig')
+
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify({
+        'status': 'success',
+        'message': 'pong!'
+    })

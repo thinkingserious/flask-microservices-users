e8d55e4fee70c6ebee8e217ad28979dc6e71a9be
Elmer Thomas <elmer@thinkingserious.com>
2017-12-25 13:28:52 -0800

    Setup Postgres

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,7 @@
 import os
+import datetime
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 # instantiate the app
 app = Flask(__name__)
@@ -7,6 +9,23 @@ app = Flask(__name__)
 # set config
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+# model
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=False, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+        self.created_at = datetime.datetime.utcnow()
 
 @app.route('/ping', methods=['GET'])
 def ping_pong():
--- project/config.py
+++ project/config.py
@@ -1,17 +1,24 @@
+import os
+
 class BaseConfig:
     """Base configuration"""
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
     DEBUG = True
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+

e6ed4aa8432ae7f29381cac805a854267c7e9802
Elmer Thomas <elmer@thinkingserious.com>
2017-12-26 00:00:19 -0800

    Add Tests

--- manage.py
+++ manage.py
@@ -1,3 +1,5 @@
+import unittest
+
 from flask_script import Manager
 from project import app, db
 
@@ -9,6 +11,15 @@ def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+
+@manager.command
+def test():
+    """Runs the tests without code coverage."""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 if __name__ == '__main__':
     manager.run()
---
+++ project/tests/test_users.py
@@ -0,0 +1,14 @@
+import json
+
+from project.tests.base import BaseTestCase
+
+class TestUserService(BaseTestCase):
+    """Tests for the Users Service."""
+
+    def test_users(self):
+        """Ensure the /ping route behaves correctly."""
+        response = self.client.get('/ping')
+        data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('pong!', data['message'])
+        self.assertIn('success', data['status'])
